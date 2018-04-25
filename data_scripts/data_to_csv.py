1e32140a9d6758e25003de9bedd07b73202cd001
MarinaIvanova239 <mar.ivanova@bk.ru>
2018-04-26 01:29:44 +0300

    add more sql queries to python script

--- data_scripts/data_to_csv.py
+++ data_scripts/data_to_csv.py
@@ -1,17 +1,36 @@
 import cx_Oracle
-connection = cx_Oracle.connect("crab_akka", "crab_akka", "srv7-furyik/oracle.net.billing.ru")
+connection = cx_Oracle.connect("crab", "snt321", "prf-cnt-db02.snt.billing.ru/db02.snt.billing.ru")
 print connection.version
 cursor = connection.cursor()
 cursor.execute("""
     SELECT external_id, from_state, to_state, to_state_time, from_state_time, in_state_time
 	FROM tmp_stats""")
 
-filename = "example.csv"
+#cursor.execute("""
+#    SELECT external_id, priority, processing_type, creation_time, exec_start_time, exec_end_time
+#	FROM tmp_orders""")
+
+#cursor.execute("""
+#    SELECT to_char(tord.creation_time, 'yyyy-mm-dd hh24:mi') as creation_time, count(*)
+#	FROM tmp_orders tord
+#	GROUP BY to_char(tord.creation_time, 'yyyy-mm-dd hh24:mi')""")
+
+
+filename = "cam_failed.csv"
 csv = open(filename, "w")
 
 for e_id, from_state, to_state, to_state_time, from_state_time, in_state_time in cursor:
-	row = e_id + "," + from_state + "," + to_state + "," + to_state_time.strftime("%Y-%m-%d %H:%M:%S") + \
-		  "," + from_state_time.strftime("%Y-%m-%d %H:%M:%S") + "," + str(in_state_time) + "\n"
+	row = e_id + "," + from_state + "," + to_state + "," + str(to_state_time) + "," + str(from_state_time) + "," + str(in_state_time) + "\n"
 	csv.write(row)
+
+#for e_id, priority, processing_type, creation_time, exec_start_time, exec_end_time in cursor:
+#	row = e_id + "," + str(priority) + "," + str(processing_type) + "," + str(creation_time) + "," + str(exec_start_time) + "," + str(exec_end_time) + "\n"
+#	csv.write(row)
+
+#for time, cnt in cursor:
+#	row = str(time) + "," + str(cnt) + "\n"
+#	csv.write(row)
+
+csv.close()
 
 connection.close()
